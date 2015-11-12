4f5890ae3a086f6c86b79e5f818d0badb03614e1
Glifery <glifery@gmail.com>
2015-11-13 02:52:46 +0300

    completely delete Maksplayer Vk Api bundle

--- src/Maxplayer/VkApiBundle/MaxplayerVkApiBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Maxplayer\VkApiBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MaxplayerVkApiBundle extends Bundle
-{
-}
--- src/Maxplayer/VkRequestBundle/DependencyInjection/MaxplayerVkRequestExtension.php
+++ src/Maxplayer/VkRequestBundle/DependencyInjection/MaxplayerVkRequestExtension.php
@@ -4,6 +4,7 @@ namespace Maxplayer\VkRequestBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MaxplayerVkRequestExtension extends Extension
+class MaxplayerVkRequestExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +25,32 @@ class MaxplayerVkRequestExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $additionalConfig = array(
+            'handlers' => array(
+                'vk_api' => array(
+                    'type' => 'stream',
+                    'path' => '%kernel.logs_dir%/vk_api.log',
+                    'level' => 'debug',
+                    'channels' => 'vk_api'
+                )
+            ),
+            'channels' => array('vk_api')
+        );
+        foreach ($container->getExtensions() as $bundleName => $extension) {
+            switch ($bundleName) {
+                case 'monolog':
+                    $container->prependExtensionConfig($bundleName, $additionalConfig);
+                    break;
+            }
+        }
     }
 }
