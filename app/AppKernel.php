28604eef55843e9ac2b6e3ad4861458b7ce2a3f9
Glifery <glifery@gmail.com>
2015-11-14 16:32:29 +0300

    Vk Api bundle full refactoring

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Maxplayer\RestBundle\MaxplayerRestBundle(),
             new Maxplayer\FrontendBundle\MaxplayerFrontendBundle(),
             new Glifery\VkOAuthTokenBundle\GliferyVkOAuthTokenBundle(),
+            new Glifery\VkApiBundle\GliferyVkApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
